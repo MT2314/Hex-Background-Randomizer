d522652bf765f1a01db2539a15b5163da64cf9d7
Mike Tkachuk <tkachuk2314@gmail.com>
2020-09-06 21:13:11 -0400

    hex randomizer function

---
+++ script.js
@@ -0,0 +1,25 @@
+//DOM Selectors
+let color = document.getElementById("container");
+//Array of Hexadecimal Bases
+let hexArray = [
+    "1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"
+];
+
+
+// Making newHex();
+let newHex = function call(){
+let randomHex = ['#'];
+
+//6 timeloop to get hex values
+for (let i=0;i<6;i++) {
+            let randomA = (hexArray[Math.floor(Math.random() *hexArray.length) ] );
+             randomHex.push(randomA);
+    
+    };
+    hex = (randomHex.join(''));
+    color.style.backgroundColor = hex;
+};
+
+//button listener
+document.getElementById("btn").addEventListener("click" , newHex );
+
